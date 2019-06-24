c3edec91ceeded684c519ec1d9f8e5f73ed27084
Lishat <30794115+Lishat@users.noreply.github.com>
2019-06-24 14:23:44 +0530

    Ajax Calls

--- Application/urls.py
+++ Application/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('home/', views.home, name="home"),
-    path('list_students/', views.list_students, name="list_students")
+    path('list_students/', views.list_students, name="list_students"),
+    path('ajax_call/', views.ajax_call, name="ajax_call"),
+    path('ajax_data/', views.ajax_data, name="ajax_data")
 ]
--- Application/views.py
+++ Application/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+import json
 # Create your views here.
 from .models import *
 
@@ -15,3 +16,18 @@ def list_students(request):
         school = School.objects.get(pk=request.POST["school"]))
         student.save()
     return render(request, "list_students.html", {"Students": Student.objects.all()})
+
+def ajax_call(request):
+    return render(request, "ajaxCall.html", {"Schools":School.objects.all()})
+
+def ajax_data(request):
+    if request.method == "POST":
+        students = Student.objects.filter(school = School.objects.get(pk=request.POST['schoolId'])).all()
+        Students_List = []
+        for i in students:
+            temp = dict()
+            temp['Name'] = i.first_name + " " + i.last_name
+            temp['Classroom'] = i.classRoom
+            Students_List.append(temp)
+        return HttpResponse(json.dumps(Students_List))
+    return HttpResponse("ERROR 404")
